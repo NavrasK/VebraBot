91f67cbbed1ad55dd02c6e8981cc336df5cdea7d
NavrasK <trailmixerstudios@gmail.com>
2020-06-10 01:34:21 -0600

    Character generation mostly complete

--- VebraCharGen.py
+++ VebraCharGen.py
@@ -19,6 +19,8 @@ class Character():
         for desc in self.inventory:
             if ("[USES: " in desc):
                 inv += " ~ "
+            elif ("Ability" in desc):
+                inv += " # "
             else:
                 inv += " > "
             inv += desc
@@ -58,18 +60,60 @@ def generateCurrency():
     return gold, silver, copper
 
 def generateAbility():
+    ability = ""
     stats = ["Move", "Power", "Thought", "Wonder", "Charm"]
-    # +1 to stat when using ability
-    # Immune / resistant to x
-    # x is an action using stat
-    # 
+    isAbility = random.randint(0, 1)
+    if (isAbility == 1):
+        ability += "Ability: "
+        isMutation = random.randint(0, 1)
+        if (isMutation == 1):
+            ability += "You are able to _____, it is an action using " + random.choice(stats)
+        else:
+            ability += "+1 to " + random.choice(stats) + " when applicable"
+    else: 
+        ability += "Item: +1 to " + random.choice(stats) + " when applicable"
+    return ability
 
 def generateConsumable():
-    print("TODO")
-    # Heal
-    # Damage
-    # Temp stat boost
-    # 
+    itemType = random.randint(0, 6)
+    level = random.randint(1, 3)
+    stats = ["Move", "Power", "Thought", "Wonder", "Charm"]
+    consumable = ""
+    if (itemType == 0):
+        #heal
+        consumable = "Item: Heals " + str(level) + "HP [USES: " + ("3" if level == 1 else "1") + "]"
+        pass
+    elif (itemType == 1):
+        #damage
+        consumable = "Item: Deals " + ("2" if level == 3 else "1") + " damage [USES: 1]"
+        pass
+    elif (itemType == 2):
+        #stat
+        consumable = "Item: Gives +1 to " + random.choice(stats) + " [USES: " + str(level) + "]"
+        pass
+    elif (itemType == 3):
+        #upgraded adventuring gear (flare, grapple, medkit, headlamp)
+        optionsConsumable = ["Flare", "Medkit, stabilizes wounds"]
+        optionsPermanent = ["Climbing kit", "Headlamp with 1 day of oil", "Hunting trap"]
+        isConsumable = random.randint(0, 1)
+        if (isConsumable == 1):
+            consumable = "Item: " + random.choice(optionsConsumable) + " [USES: " + str(level) + "]"
+        else:
+            consumable = "Item: " + random.choice(optionsPermanent)
+    elif (itemType == 4):
+        #relationship booster
+        consumable = "Item: Gives +1 Relationship [USES: " + str(level) + "]"
+    elif (itemType == 5):
+        #resource point generator (food, potion, herbs)
+        consumable = "Item: Grants a ______ resource point [USES: " + ("4" if level == 3 else "3") + "]"
+    elif (itemType == 6):
+        #valuable item (gem, broken artifact, trinket)
+        options = ["gem", "broken artifact", "trinket"]
+        consumable = "Item: A valuable " + random.choice(options)
+    return consumable
+
+def generateAdventurerGear():
+    return ["Bedroll", "Rations [USES: 3]", "Waterskin", "1 Torch", "10' Rope", "Tinderbox"]
 
 def generateCharacter(mf, ff, l):
     statValues = [2, 1, 0, 0, -1]
@@ -90,15 +134,15 @@ def generateCharacter(mf, ff, l):
     vebran.name = generateName(vebran.gender, mf, ff, l)
     vebran.gold, vebran.silver, vebran.copper = generateCurrency()
     for _ in range(3):
-        vebran.inventory.append(generateAbility)
+        vebran.inventory.append(generateAbility())
     for _ in range(1 + random.randint(0, 1)):
-        vebran.inventory.append(generateConsumable)
+        vebran.inventory.append(generateConsumable())
+    vebran.inventory += generateAdventurerGear()
     print(vebran)
 
 # 3 random special actions / items
 # 1-2 single use items
-# Adventurers pack (1 bedroll, 3 days of rations, a filled waterskin, a torch, 10 feet of rope, and a tinderbox)
-# random money 1-5g
+# Adventurers pack 
 
 # player will have to choose class, traits, relationship stats, physical appearance, and fluff / backstory
 
